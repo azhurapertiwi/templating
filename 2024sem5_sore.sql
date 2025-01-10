4a5a88e08fec64d52d567d8fa355e981d8a19daa
agoengBani <agungsyabani99@gmail.com>
2025-01-11 03:25:46 +0700

    update db

--- 2024sem5_sore.sql
+++ 2024sem5_sore.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 06, 2025 at 05:13 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- Waktu pembuatan: 10 Jan 2025 pada 21.25
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `anggota`
+-- Struktur dari tabel `anggota`
 --
 
 CREATE TABLE `anggota` (
@@ -37,16 +37,17 @@ CREATE TABLE `anggota` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `anggota`
+-- Dumping data untuk tabel `anggota`
 --
 
 INSERT INTO `anggota` (`nik`, `nama`, `email`, `no_hp`, `alamat`, `foto`) VALUES
+('1234098754278903', 'Nama kedua', 'kedua@gmail.com', '87234723843', 'JL. SILIWANGI', '58083ccc01f63bbd3da6bf602c8be9b2.png'),
 ('563456456', 'Nama tiga', 'tiga@gmail.com', '7456456754', 'JL.SUDIRMAN', '152e96c3d05d0a947d6e24407460b7f4.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `buku`
+-- Struktur dari tabel `buku`
 --
 
 CREATE TABLE `buku` (
@@ -63,18 +64,18 @@ CREATE TABLE `buku` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `buku`
+-- Dumping data untuk tabel `buku`
 --
 
 INSERT INTO `buku` (`kode`, `judul`, `kategori_kode`, `isbn`, `penulis`, `penerbit_kode`, `tahun`, `cover`, `bahasa`, `sinopsis`) VALUES
 ('111', 'aaaaaa', 'CMC', '101010101', 'lkjlkjlkj', '23', '2025-01-02', '52ae960442c9aef06a8b5b9b8f5d27da.jpg', 'Indonesia', 'lkjhlkjhlkjhlkjh'),
 ('123', 'Buku baru', 'NVL', '34562345', 'penulis baru', 'ERL', '2025-01-05', 'c54182246d5faebeef4a89d97f5115a0.jpg', 'Indonesia', 'Coba input buku baru'),
-('123123', 'qweqwe', '111', '098098098', 'aqwaqwaqw', '111', '2025-01-01', '0e21776847f48467bc5b1caf72bcb098.jpg', 'Indonesia', 'aaaaaaaaa');
+('123123', 'qweqwe', 'CMC', '098098098', 'aqwaqwaqw', '111', '2025-01-01', 'f0a615281dd8bda5446bd3184fb9d4e9.png', 'Indonesia', 'aaaaaaaaa');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `detail_transaksi`
+-- Struktur dari tabel `detail_transaksi`
 --
 
 CREATE TABLE `detail_transaksi` (
@@ -85,17 +86,25 @@ CREATE TABLE `detail_transaksi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `detail_transaksi`
+-- Dumping data untuk tabel `detail_transaksi`
 --
 
 INSERT INTO `detail_transaksi` (`id`, `id_transaksi`, `nik_anggota`, `kode_buku`) VALUES
 (1, '1', '563456456', '123'),
-(2, '1', '563456456', '123123');
+(2, '1', '563456456', '123123'),
+(4, '3', '1234098754278903', '111'),
+(5, '3', '1234098754278903', '123'),
+(6, '4', '563456456', '111'),
+(7, '4', '563456456', '123'),
+(8, '4', '563456456', '123123'),
+(9, '5', '1234098754278903', '111'),
+(10, '5', '1234098754278903', '123'),
+(11, '5', '1234098754278903', '123123');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kategori`
+-- Struktur dari tabel `kategori`
 --
 
 CREATE TABLE `kategori` (
@@ -104,18 +113,17 @@ CREATE TABLE `kategori` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `kategori`
+-- Dumping data untuk tabel `kategori`
 --
 
 INSERT INTO `kategori` (`kategori_kode`, `kategori_nama`) VALUES
-('111', 'asasasa'),
-('CMC', 'COMICC'),
+('CMC', 'COMIC'),
 ('NVL', 'NOVEL');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `penerbit`
+-- Struktur dari tabel `penerbit`
 --
 
 CREATE TABLE `penerbit` (
@@ -125,7 +133,7 @@ CREATE TABLE `penerbit` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `penerbit`
+-- Dumping data untuk tabel `penerbit`
 --
 
 INSERT INTO `penerbit` (`penerbit_kode`, `penerbit_nama`, `penerbit_alamat`) VALUES
@@ -135,54 +143,36 @@ INSERT INTO `penerbit` (`penerbit_kode`, `penerbit_nama`, `penerbit_alamat`) VAL
 ('222', 'wewewew', 'aaaaaaa'),
 ('23', 'Dua agiT', 'Jalan Angka'),
 ('BPU', 'Bentang Pustaka', 'JL. BUDI KEMULIAAN'),
-('ERL', 'ERLANGGA', 'JL.SUDIRMAN');
+('ERL', 'ERLANGGA', 'JL.SUDIRMAN'),
+('PB', 'Penerbit Baru', 'Jl. Sukajadi');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transaksi`
+-- Struktur dari tabel `transaksi`
 --
 
 CREATE TABLE `transaksi` (
   `id` int(11) NOT NULL,
   `nik_anggota` varchar(16) NOT NULL,
-  `tanggal_pinjam` date NOT NULL,
-  `tanggal_kembali` date DEFAULT NULL
+  `tgl_pinjam` date NOT NULL,
+  `tgl_kembali` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `transaksi`
+-- Dumping data untuk tabel `transaksi`
 --
 
-INSERT INTO `transaksi` (`id`, `nik_anggota`, `tanggal_pinjam`, `tanggal_kembali`) VALUES
-(1, '563456456', '2025-01-05', NULL);
+INSERT INTO `transaksi` (`id`, `nik_anggota`, `tgl_pinjam`, `tgl_kembali`) VALUES
+(1, '563456456', '2025-01-05', '2025-01-11'),
+(3, '1234098754278903', '2025-01-10', '2025-01-11'),
+(4, '563456456', '2025-01-11', NULL),
+(5, '1234098754278903', '2025-01-11', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transaksi_kembali`
---
-
-CREATE TABLE `transaksi_kembali` (
-  `nama` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transaksi_pinjam`
---
-
-CREATE TABLE `transaksi_pinjam` (
-  `kode` varchar(20) NOT NULL,
-  `nama` varchar(30) NOT NULL,
-  `alamat` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -192,7 +182,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`) VALUES
@@ -204,7 +194,7 @@ INSERT INTO `users` (`id`, `username`, `password`) VALUES
 --
 
 --
--- Indexes for table `anggota`
+-- Indeks untuk tabel `anggota`
 --
 ALTER TABLE `anggota`
   ADD PRIMARY KEY (`nik`),
@@ -212,67 +202,61 @@ ALTER TABLE `anggota`
   ADD UNIQUE KEY `no_hp` (`no_hp`);
 
 --
--- Indexes for table `buku`
+-- Indeks untuk tabel `buku`
 --
 ALTER TABLE `buku`
   ADD PRIMARY KEY (`kode`);
 
 --
--- Indexes for table `detail_transaksi`
+-- Indeks untuk tabel `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `kategori`
+-- Indeks untuk tabel `kategori`
 --
 ALTER TABLE `kategori`
   ADD PRIMARY KEY (`kategori_kode`),
   ADD UNIQUE KEY `nama` (`kategori_nama`);
 
 --
--- Indexes for table `penerbit`
+-- Indeks untuk tabel `penerbit`
 --
 ALTER TABLE `penerbit`
   ADD UNIQUE KEY `kode` (`penerbit_kode`);
 
 --
--- Indexes for table `transaksi`
+-- Indeks untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `transaksi_pinjam`
---
-ALTER TABLE `transaksi_pinjam`
-  ADD UNIQUE KEY `kode` (`kode`);
-
---
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `detail_transaksi`
+-- AUTO_INCREMENT untuk tabel `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT for table `transaksi`
+-- AUTO_INCREMENT untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
